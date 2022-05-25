44551215d0ae4e387377fc15e15ca628ab068112
Jonas Wolfram <jonas.wolfram@me.com>
2022-05-25 20:26:06 +0200

    NavBar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import styled from "styled-components";
-
-const NavContainer = styled.div``;
+import * as React from "react";
 
 function NavBar() {
-  return <div className="nav--container">NavBar</div>;
+  return <></>;
 }
 
 export default NavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import NavBar from "../components/NavBar/NavBar";
+import WithSubnavigation from "../components/NavBar/WithSubnavigation";
 
 const Home: NextPage = () => {
   return (
     <div>
       <NavBar />
+      <WithSubnavigation />
     </div>
   );
 };
