3dfb83bb576dbfe3ef57e8c9e3dc8f51c2acecb9
Jonas Wolfram <jonas.wolfram@me.com>
2022-05-25 20:01:33 +0200

    first commit

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavContainer = styled.div``;
+
+function NavBar() {
+  return <div className="nav--container">NavBar</div>;
+}
+
+export default NavBar;
